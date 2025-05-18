9c5f323923d8837d8ddea20c4d63454b9bc58bbb
QuielNasci <124948821+QuielNasci@users.noreply.github.com>
2025-05-18 17:03:55 -0300

    Add files via upload

---
+++ assets/js/auth.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Formulário de Cadastro
+  const form = document.getElementById("signup-form");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); // Evitar o comportamento padrão de envio do formulário
+
+    // Pegando os valores dos campos do formulário
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("new-email").value;
+    const senha = document.getElementById("new-password").value;
+    const telefone = document.getElementById("telefone").value;
+
+    // Criando a URL com os parâmetros para o Google Apps Script
+    const url = `https://script.google.com/macros/s/AKfycbwNpa2A0aoW8zaepdn7KVCdCU2tqB4v9bAY6kIuoufX_nz2pZOUZFYG9f3_2_d0WReM/exec?action=cadastro&nome=${encodeURIComponent(nome)}&email=${encodeURIComponent(email)}&senha=${encodeURIComponent(senha)}&telefone=${encodeURIComponent(telefone)}`;
+
+    // Enviando a requisição GET para o Google Apps Script
+    fetch(url)
+      .then(response => response.text())
+      .then(data => {
+        // Exibir mensagem de sucesso ou erro
+        document.getElementById("mensagem").textContent = data;
+      })
+      .catch(error => {
+        // Exibir mensagem de erro
+        document.getElementById("mensagem").textContent = "Erro ao cadastrar usuário: " + error.message;
+      });
+  });
+});
